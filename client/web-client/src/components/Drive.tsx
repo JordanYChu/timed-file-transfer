1c96b27205d7fc1c9bfaa2a5562ef1d01d833f01
JordanYChu <jordan.yn.chu@gmail.com>
2025-05-04 22:01:34 -0400

    Drive Accurately portrays current files

--- client/web-client/src/components/Drive.tsx
+++ client/web-client/src/components/Drive.tsx
@@ -1,41 +1,36 @@
 import { useContext } from "react";
 import "../assets/drive.css"
-import { fileTypes } from "../fileMapping";
+import { fileTypes, fileTypeMapping } from "../fileMapping";
 import { FileSystemContext } from "../FileSystemProvider";
 
 const Drive = () => {
-    const { usedStorage, storage } = useContext(FileSystemContext).systemInfo;
+    const { files, usedStorage, storage } = useContext(FileSystemContext).systemInfo;
+
+    const fileCounts = Object.fromEntries(fileTypes.map(key => [key, 0]));
+    for (let file of files) {
+        const category = fileTypeMapping[file.fileExtension].category;
+        if (!category) continue;
+        fileCounts[category]++;
+    }
     return (
         <div className="drive-container">
             <div className="drive float">
                 <div className="drive-wheel">
                     <div className="drive-space">
-                        <div className="space">{usedStorage} b</div>
+                        <div className="space">{Math.floor(usedStorage / 1024 / 1024)} MB</div>
                         <div>used</div>
                     </div>
                 </div>
             </div>
             <div className="info-card-sidebar">
-                <div className="info-box">
-                    <div>Files</div>
-                    <div>1</div>
-                </div>
-                <div className="info-box">
-                    <div>Images</div>
-                    <div>2</div>
-                </div>
-                <div className="info-box">
-                    <div>Videos</div>
-                    <div>3</div>
-                </div>
-                <div className="info-box">
-                    <div>Documents</div>
-                    <div>5</div>
-                </div>
-                <div className="info-box">
-                    <div>Archives</div>
-                    <div>5</div>
-                </div>
+                {Object.entries(fileCounts).map(([type, count]) => {
+                    return (
+                        <div className="info-box">
+                            <div>{type}</div>
+                            <div>{count}</div>
+                        </div>
+                    )
+                })}
             </div>
         </div>
     )
