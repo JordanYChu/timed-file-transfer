d24da70b15ef40c8576faad1103cb2a515fdfe1b
JordanYChu <jordan.yn.chu@gmail.com>
2025-05-07 18:20:02 -0400

    Added Login Screen

--- client/web-client/src/Login.tsx
+++ client/web-client/src/Login.tsx
@@ -36,11 +36,27 @@ const Login = () => {
     return (
         <div className='login-screen'>
             <div className='intro-section'>
-
+                <h1 className='title'>Timed File Transfer</h1>
+                <p>
+                    This app helps you manage temporary files by automatically deleting them after a period of time.
+                </p>
+                <p>
+                    You can upload or transfer files and assign a lifespan to each. Once their time is up, they'll be removed saving space and reducing clutter.
+                </p>
+                <ul>
+                    <li>Prevent storage overflow</li>
+                    <li>Automate cleanup of old files</li>
+                    <li>Time-controlled transfers</li>
+                </ul>
+                <p>
+                    "Keep only what matters. Let the rest go — automatically."
+                </p>
             </div>
-            <div>
-                <h1>Create an Account or Login</h1>
-                <button onClick={handleGoogleAuth}>click me</button>
+            <div className='login-section'>
+                <h1><strong>Login</strong></h1>
+                <span style={{ fontSize: "small" }}>Use google to access your account</span>
+                <hr style={{ margin: "1rem 0", border: "none", borderTop: "2px solid var(--primary)" }} />
+                <button className="signup" onClick={handleGoogleAuth}>Login with Google</button>
             </div>
         </div>
     )
